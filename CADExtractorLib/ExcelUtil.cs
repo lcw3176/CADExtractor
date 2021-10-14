fa39273fffa5f32e902b255f322b341ed8281b94
joebrooks <lcw3176@naver.com>
2021-10-14 17:44:00 +0900

    데이터 표시 세부화

--- CADExtractorLib/ExcelUtil.cs
+++ CADExtractorLib/ExcelUtil.cs
@@ -21,7 +21,7 @@ namespace CADExtractorLib
         /// </summary>
         /// <param name="layerList">추출한 캐드 레이어 이름</param>
         /// <param name="areaList">polyline으로 닫혀진 면적</param>
-        public void AddData(List<string> layerList, List<string> areaList)
+        public void AddData(List<string> layerList, List<double> areaList)
         {
             Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
             
@@ -33,24 +33,58 @@ namespace CADExtractorLib
 
             try
             {
-
+                Dictionary<string, int> layerColumnDict = new Dictionary<string, int>();
+                Dictionary<string, int> layerRowDict = new Dictionary<string, int>();
+                Dictionary<string, double> areaValueDict = new Dictionary<string, double>();
+                
                 app = new Excel.Application();
                 wb = app.Workbooks.Open(path);
                 ws = wb.Worksheets.get_Item("Sheet1") as Excel.Worksheet;
 
+                int index = 1;
+
+                foreach(string i in layerList)
+                {
+                    if (!layerColumnDict.ContainsKey(i))
+                    {
+                        layerColumnDict.Add(i, index);
+                        layerRowDict.Add(i, 1);
+                        areaValueDict.Add(i, 0);
+
+                        ws.Cells[1, index++] = i;
+                    }
+
+                    continue;
+
+                }
+
                 for (int i = 0; i < layerList.Count; i++)
                 {
-                    ws.Cells[i + 1, 1] = layerList[i];
-                    ws.Cells[i + 1, 2] = areaList[i];
+                    ws.Cells[++layerRowDict[layerList[i]], layerColumnDict[layerList[i]]] = areaList[i];
+
+                    areaValueDict[layerList[i]] += areaList[i];
                 }
+
+                int rowIndex = 1;
+                int columnIndex = layerColumnDict.Count;
+
+                foreach(string i in layerColumnDict.Keys)
+                {
+                    ws.Cells[rowIndex, columnIndex + 2] = i;
+                    ws.Cells[rowIndex, columnIndex + 3] = areaValueDict[i];
+                    ws.Cells[rowIndex++, columnIndex + 4] = Math.Round(areaValueDict[i], MidpointRounding.AwayFromZero).ToString();
+                }
+
 
 
                 ed.WriteMessage("\nComplete");
             }
 
-            catch
+            catch(Exception ex)
             {
+                
                 ed.WriteMessage("\nError!");
+                ed.WriteMessage(ex.ToString());
             }
 
             finally
